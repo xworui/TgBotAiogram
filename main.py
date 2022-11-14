77b6c01751cfa36bfc4a21f3cafe07b81b0d747e
gaidukov <gaidukov_nikita@mail.ru>
2022-11-14 23:25:58 +0300

    Сделали функции, при который бот отвечает на все сообщения

---
+++ main.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = "5774623601:AAE7Mjcq5frB1nhWAWfw6xN5QODteAyWyF4"
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot) #диспетчер, анализирует все входящие запросы
+
+@dp.message_handler()                             # отвечает на все сообщения
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
